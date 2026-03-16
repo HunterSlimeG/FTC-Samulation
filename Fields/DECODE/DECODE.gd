19c55521d002d9b98c0181471c110b6af0223caa
HunterSlimeG <thesamsilva@gmail.com>
2026-03-15 22:09:03 -0400

    Driver Mappings

--- Fields/DECODE/DECODE.gd
+++ Fields/DECODE/DECODE.gd
@@ -7,6 +7,7 @@ var cam = 0
 # Called when the node enters the scene tree for the first time.
 var tag = "res://Fields/DECODE/AprilTags/AprilTag ("+str(randi_range(1, 3))+").png"
 func _ready() -> void:
+	GUIDE.enable_mapping_context(load("res://Drivers/Driver1.tres"))
 	reload()
 
 func _process(delta: float) -> void:
