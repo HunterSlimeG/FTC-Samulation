b087749ef99721f4d46b2f609038212c22cabc4f
HunterSlimeG <thesamsilva@gmail.com>
2026-07-12 22:18:00 -0400

    Controller Menu

--- Menu/Cursor/Cursor.gd
+++ Menu/Cursor/Cursor.gd
@@ -2,15 +2,23 @@ extends Node2D
 
 var con: Node
 
-@export var active = true
+@export var active = false
 @export var device = 0
 
 var frames: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	match device:
+		1:
+			$MeshInstance2D.modulate = Color(0, 0, 1)
+		2:
+			$MeshInstance2D.modulate = Color(1, 0, 0)
+		3:
+			$MeshInstance2D.modulate = Color(0, 1, 0)
+		4:
+			$MeshInstance2D.modulate = Color(1, 0, 1)
 	$Label.text = str(device)
-	exit()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,14 +26,22 @@ func _process(delta: float) -> void:
 	position+=Global.cursorContexts[device-1].mappings[1].action.value_axis_2d*500*delta
 	frames += 1
 	if frames%60==0:
+		con = self
 		for i in get_all_selectables(owner):
-			if i.get_global_rect().has_point(global_position) and i.visible:
+			if i.get_global_rect().has_point(global_position) and i.visible and i.get_parent().visible and (not i is Panel and not i is Label and (not i is Container or i is TabContainer)):
 				con = i
-				con.grab_focus()
-				print(con)
+				$Panel.visible = true
+				$Panel.global_position = con.get_global_rect().position
+				$Panel.size = con.get_global_rect().size
 				break
+		if con==self:
+			$Panel.visible = false
 
 func _input(event: InputEvent) -> void:
+	if not Global.cursorContexts[device-1].mappings[1].action.value_axis_2d.is_zero_approx() and not active:
+		active = true
+		visible = true
+		
 	if Global.cursorContexts[device-1].mappings[0].action.value_bool and active:
 		if con!=self:
 			if con is CheckBox:
@@ -36,21 +52,18 @@ func _input(event: InputEvent) -> void:
 					con.pressed.emit()
 					con.button_pressed = true
 			elif con is ItemList:
-				var it: int = con.get_item_at_position(global_position, true)
-				if not con.is_selected(it):
+				var pos = Vector2(position.x, position.y-40)-con.position
+				var it: int = con.get_item_at_position(pos, false)
+				if not con.is_selected(it) and it>-1:
 					con.select(it)
 					con.item_selected.emit(it)
 			elif con is OptionButton:
 				con.show_popup()
 			elif con is Button:
 				con.pressed.emit()
-
-func enter(node):
-	con = node.get_parent()
-	print(con)
-
-func exit(node = self):
-	con = self
+			elif con is TabContainer:
+				print(con.current_tab)
+				con.current_tab = con.get_tab_idx_at_point(global_position)
 
 func get_all_selectables(node: Node):
 	var nodes : Array = []
